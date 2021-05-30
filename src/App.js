ea0b0316334a35e73b7b6d6a8171b7262456f6f5
jovannil94 <jovanniluna@pursuit.org>
2021-05-30 15:03:41 -0400

    created Homepage and Raffles

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+import Homepage from './components/Homepage';
+import Raffle from './components/Raffles';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Switch>
+        <Route exact path={'/'} component={Homepage}></Route>
+        <Route exact path={'/raffles'} component={Raffle}></Route>
+      </Switch>
     </div>
   );
 }
