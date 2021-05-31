6af90c3e288a17de137582450b419b875ce2b6ec
jovannil94 <jovanniluna@pursuit.org>
2021-05-30 20:05:37 -0400

    added helper component

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,44 +1,16 @@
 import React, { useState, useEffect } from 'react';
-import { getAPI } from '../util/getAPI';
-import axios from 'axios';
+import AllRafflesDisplay from '../helper/allRafflesDisplay';
+
 
 const Homepage = () => {
-    const [allRaffles, setAllRaffles] = useState([]);
     
-    const displayRaffles = allRaffles.map((raffle) => (
-        <div key={raffle.id}>
-            <h1>{raffle.name}</h1>
-            <p>Created on: {raffle.created_at}</p>
-            {raffle.winner_id ?
-                <p>Winner Id: {raffle.winner_id}</p>
-                : <p>Winner Id: No winner yet</p>
-            }
-            {raffle.raffled_at ?
-                <p>Raffled on: {raffle.raffled_at}</p>
-                : <p>Not raffled yet</p>
-            }
-        </div>
-    ))
-    
-    useEffect(() => {
-        const API = getAPI();
-        const fetchRaffles = async () => {
-            try {
-                let res = await axios.get(`${API}/raffles`);
-                setAllRaffles(res.data.payload);
-            } catch (error) {
-                console.log(error);
-            }
-        }
-        fetchRaffles();
-    }, []);
 
     return (
         <div className="homeContainer">
             <h1>Homepage</h1>
             <div>
-                <h2>All Raffles</h2>
-                {displayRaffles}
+                <h2>All Raffles: </h2>
+                <AllRafflesDisplay/>
             </div>
         </div>
     )
---
+++ src/helper/allRafflesDisplay.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react';
+import { getAPI } from '../util/getAPI';
+import axios from 'axios';
+
+const AllRafflesDisplay = () => {
+    const [allRaffles, setAllRaffles] = useState([]);
+    
+    
+    useEffect(() => {
+        const API = getAPI();
+        const fetchRaffles = async () => {
+            try {
+                let res = await axios.get(`${API}/raffles`);
+                setAllRaffles(res.data.payload);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+        fetchRaffles();
+    }, []);
+
+    const displayRaffles = allRaffles.map((raffle) => (
+        <div key={raffle.id}>
+            <h2>{raffle.name}</h2>
+            <p>Created on: {raffle.created_at}</p>
+            {raffle.winner_id ?
+                <p>Winner Id: {raffle.winner_id}</p>
+                : <p>Winner Id: No winner yet</p>
+            }
+            {raffle.raffled_at ?
+                <p>Raffled on: {raffle.raffled_at}</p>
+                : <p>Not raffled yet</p>
+            }
+        </div>
+    ))
+    return (
+        <div className="rafflesDisplay">
+            {displayRaffles}
+        </div>
+    )
+}
+
+export default AllRafflesDisplay;
