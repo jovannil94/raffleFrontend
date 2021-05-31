e147709e0215fc83dec8b8f332b6c700a21b855f
jovannil94 <jovanniluna@pursuit.org>
2021-05-30 20:27:25 -0400

    create raffle working

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import AllRafflesDisplay from '../helper/allRafflesDisplay';
+import React from 'react';
+import AllRafflesDisplay from '../helper/AllRafflesDisplay';
+import CreateRaffle from '../helper/CreateRaffle';
 
 
 const Homepage = () => {
-    
 
     return (
         <div className="homeContainer">
             <h1>Homepage</h1>
-            <div>
+            <div className="homeRaffles">
+                <CreateRaffle/>
                 <h2>All Raffles: </h2>
                 <AllRafflesDisplay/>
             </div>
---
+++ src/helper/CreateRaffle.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { getAPI } from '../util/getAPI';
+import axios from 'axios';
+import { useInputs } from '../util/useInputs';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+
+const CreateRaffle = () => {
+    const nameContext = useInputs("");
+    const tokenContext = useInputs("");
+    const API = getAPI();
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            await axios.post(`${API}/raffles`, {
+               name: nameContext.value,
+               secret_token: tokenContext.value
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    }
+
+    return (
+        <form className="createForm" onSubmit={handleSubmit}>
+            <TextField id="outlined-basic" color='secondary' label="Raffle Name" variant="outlined" autoFocus required {...nameContext}/>
+            <TextField id="outlined-basic" color='secondary' label="Raffle Secret Token" variant="outlined" autoFocus required {...tokenContext}/>
+            <p>You must remember the Raffle Token because it will be asked when picking a winner</p>
+            <Button variant="contained" color='secondary' type="submit">Create New Raffle</Button>
+        </form>
+    )
+}
+
+export default CreateRaffle;
