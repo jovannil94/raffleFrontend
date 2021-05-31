81ad8d13d736c94f3be0d8083cf370fe6d6d02fe
jovannil94 <jovanniluna@pursuit.org>
2021-05-31 14:14:54 -0400

    adds context to pickwinner.js

--- src/components/PickWinner.js
+++ src/components/PickWinner.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { getAPI } from '../util/getAPI';
+import { useInputs } from '../util/useInputs';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const PickWinner = () => {
+    const tokenContext = useInputs("");
+
     return (
-        <div>
-            winner
+        <div className="winnerContainer">
+            <h1>Pick a Winner</h1>
+            <form className="winnerForm" onSubmit={handleSubmit}>
+                <TextField id="outlined-basic" color='secondary' label="Secret Token" variant="outlined" autoFocus required {...tokenContext}/>
+                <Button variant="contained" color='secondary' type="submit">Submit</Button>
+            </form>
+            <div className="winnerTokenContext">
+                <h2>Secret Token</h2>
+                <p>The secret token used when creating the raffle must be provided.</p>
+            </div>
         </div>
     )
 }
